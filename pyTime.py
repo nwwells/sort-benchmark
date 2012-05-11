bd45411970856564d89297660069f6ff3698ef82
Nathan Wells <nwwells@gmail.com>
2012-05-11 11:29:43 -0600

    added loop

--- pyTime.py
+++ pyTime.py
@@ -1,7 +1,8 @@
 import random
 import time
-array = [random.randint(1,100000) for i in xrange(10000)]
-start = time.time() * 1000000
-array.sort()
-end = time.time() * 1000000
-print "Elapsed time: %i microseconds" % (end - start)
+for i in xrange(100):
+  array = [random.randint(1,100000) for i in xrange(10000)]
+  start = time.time() * 1000000
+  array.sort()
+  end = time.time() * 1000000
+  print "Elapsed time: %i microseconds" % (end - start)
--- time.js
+++ time.js
@@ -1,6 +1,9 @@
-var array = []
-while(array.push(Math.floor((Math.random()*100000)+1)) < 10000)
-var start = Date.now()
-array.sort()
-var end = Date.now()
-console.log("Elapsed time: %d milliseconds", end - start)
+var times = 100;
+while(times--) {
+  var array = []
+  while(array.push(Math.floor((Math.random()*100000)+1)) < 10000)
+  var start = Date.now()
+  array.sort()
+  var end = Date.now()
+  console.log("Elapsed time: %d milliseconds", end - start)
+}

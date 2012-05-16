5b030bfef93bc1d82e9702665f2bf575ecf8a90c
Stephen M. McQuay <smcquay@fusionio.com>
2012-05-16 15:45:36 -0600

    improved (arguably) legibility of output

--- results.py
+++ results.py
@@ -7,17 +7,27 @@ from stats import running_stat
 
 
 start = time.time()
-program = sys.argv[1:]
-p = subprocess.check_output(program)
-number_pattern = re.compile(r'(\d+)')
+program = ['./xtime']
+program.extend(sys.argv[1:])
+p = subprocess.check_output(program, stderr=subprocess.STDOUT)
 total_time = time.time() - start
 
+number_pattern = re.compile(r'(\d+)')
+memory_pattern = re.compile(r'(\d+kB)')
+
 rs = running_stat()
-c = 0
-for i in p.split('\n'):
+output = p.strip().split('\n')
+for i in output[:-1]:
     number = number_pattern.search(i)
     if number is not None:
         number = number.group()
         rs.push(float(number))
-    c += 1
-print('{0} {1} total time: {2}'.format(program, rs, total_time))
+rams = memory_pattern.search(output[-1]).group(1)
+print('{name:25s} ram: {ram:>10}   min: {rs.min:10}   max: {rs.max:10}'
+        ' mean: {rs.mean:10.1f} stddev: {rs.stddev:10.1f} '
+        ' total time: {total:0.4f}'.format(**{
+    'name': ' '.join(program[1:]),
+    'total': total_time,
+    'rs': rs,
+    'ram': rams,
+}))

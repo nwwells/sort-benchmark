60d206342ca787ee6a4cfadf09c27c2236266e34
Stephen M. McQuay <smcquay@fusionio.com>
2012-05-16 17:50:24 -0600

    Added a (kinda crappy) go example

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 CXXFLAGS=-Wall -O3
 
-all: time JavaTime.class
+all: time JavaTime.class gotime
 
 time: time.cpp
+
+gotime: time.go
+	go build -o gotime time.go
 
 JavaTime.class: JavaTime.java
 	javac JavaTime.java
@@ -13,6 +16,7 @@ run: all
 	@python results.py python python-time.py
 	@python results.py node node-time.js
 	@python results.py groovy groovyTime.groovy
+	@python results.py ./gotime
 
 clean:
-	@rm -vf JavaTime.class ./time
+	@rm -vf JavaTime.class time gotime
---
+++ time.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "math/rand"
+import "sort"
+import "time"
+
+const MaxLength = 10000
+const MaxLoops = 100
+
+func main() {
+	nums := make([]int, MaxLength)
+
+	for iter := 0; iter < MaxLoops; iter++ {
+		for i := range nums {
+			nums[i] = rand.Int()
+		}
+        t0 := time.Now()
+		sort.Ints(nums)
+        t1 := time.Now()
+        delta := t1.Sub(t0)
+        fmt.Printf("Elapsed time: %v microseconds\n", delta.Nanoseconds() / 1000)
+	}
+}
